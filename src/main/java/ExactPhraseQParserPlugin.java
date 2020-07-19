5f68503bf6b0308feb953087f5ade02130bd6fb1
kaya.ota@gmail.com <0246kaya>
2020-07-19 23:29:47 +0900

    edit ExactPhraseQParserPlugin

--- src/main/java/ExactPhraseQParserPlugin.java
+++ src/main/java/ExactPhraseQParserPlugin.java
@@ -6,6 +6,9 @@ import org.apache.solr.search.QParserPlugin;
 public class ExactPhraseQParserPlugin extends QParserPlugin {
     @Override
     public QParser createParser(String qstr, SolrParams localParams, SolrParams params, SolrQueryRequest req) {
-        return null;
+        System.out.printf("[ExactPhraseQParserPlugin] createParser is called {qstr = %s; localParams = %s; params = %s ; req = %s}\n",
+                qstr, localParams.toLocalParamsString(), params.toLocalParamsString(),req.getParamString() );
+
+        return new ExactPhraseQParser(qstr, localParams, params, req);
     }
 }
